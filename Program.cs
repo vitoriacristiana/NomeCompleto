85389b9a17e129977ea081150eeb9afdf634fc32
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-29 14:56:32 -0300

    resolução

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+             String nome, sobrenome;
+            Console.Write ("Digite seu nome .. ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome..");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"Nome completo: {nome} {sobrenome}");  
+            Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+
+
+
+
+
+
+        }
+    }
+}
